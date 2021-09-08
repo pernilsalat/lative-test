40a99fd2a6545d37ab98a5e0c1a6cfd296bdf93f
Javier Pardo <xavi@lative.io>
2021-09-08 08:01:55 +0100

    remove sass

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="formPanel">
+        <Container className="p-3">
+          <Nav>
+            <Nav.Item className="logo">
+              <img src='/images/lative-logo.png' alt='Lative Software' />
+            </Nav.Item>
+          </Nav>
+          <h1 className="header">Growth Ranking of U.S. States</h1>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Year</Form.Label>
+                  <Form.Control as="select">
+                    <option>2019</option>
+                    <option>2018</option>
+                    <option>2017</option>
+                    <option>2016</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Measure</Form.Label>
+                  <Form.Control as="select">
+                    <option>Household Income</option>
+                    <option>Population</option>
+                    <option>Property Value</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+              <Form.Group>
+                  <Form.Label>Growth Period</Form.Label>
+                  <Form.Control as="select">
+                    <option>1 Year</option>
+                    <option>2 Years</option>
+                    <option>3 Years</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+      </div>
+      <div className="results">
+        <Container>
+          <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+          </div>
+        </Container>
+      </div>
+    </div>
+    );
+}
+
+export default App;
